10bc505dab6e89f4ec0b3b91524c55800f5219a5
kavladst <47891202+kavladst@users.noreply.github.com>
2019-08-13 16:23:37 +0300

    Изменение is_less

--- PolynomialOrder.h
+++ PolynomialOrder.h
@@ -9,34 +9,38 @@ namespace grobner {
         MonomialOrder<T, TNumberOfVariables>& get_mon_order() { return mon_order_; }
 
         PolynomialOrder(const MonomialOrder<T, TNumberOfVariables>& tmp_mon_ord) {
-            mon_order_ = tmp_mon_ord;
+          mon_order_ = tmp_mon_ord;
         }
 
-        bool is_less(Polynomial<T, TNumberOfVariables>& pol1,
-                     Polynomial<T, TNumberOfVariables>& pol2) {
-            std::sort((pol1.get_monomials()).begin(), (pol1.get_monomials()).end(),
-                      [this](Monomial<T, TNumberOfVariables> const& mon1,
-                             Monomial<T, TNumberOfVariables> const& mon2) {
-                          return mon_order_.is_less(mon1, mon2);
-                      });
+        bool is_less(Polynomial<T, TNumberOfVariables> const& pol1,
+                     Polynomial<T, TNumberOfVariables> const& pol2) const {
 
-            std::sort((pol2.get_monomials()).begin(), (pol2.get_monomials()).end(),
-                      [this](Monomial<T, TNumberOfVariables> const& mon1,
-                             Monomial<T, TNumberOfVariables> const& mon2) {
-                          return mon_order_.is_less(mon1, mon2);
-                      });
+          std::vector<const Monomial<T, TNumberOfVariables>&> view_poly1 = pol1.get_monomials();
+          std::vector<const Monomial<T, TNumberOfVariables>&> view_poly2 = pol2.get_monomials();
 
-            auto it1 = pol1.rbegin();
-            auto it2 = pol2.rbegin();
-            while (it1 != pol1.rend() && it2 != pol2.rend()) {
-                if (mon_order_.is_less(*it1, *it2))
-                    return true;
-                if (mon_order_.is_less(*it2, *it1))
-                    return false;
-                ++it1;
-                ++it2;
-            }
-            return pol1.amount_of_monomials() < pol2.amount_of_monomials();
+          std::sort(view_poly1.begin(), view_poly1.end(),
+                    [this](Monomial<T, TNumberOfVariables> const& mon1,
+                           Monomial<T, TNumberOfVariables> const& mon2) {
+                      return mon_order_.is_less(mon1, mon2);
+                    });
+
+          std::sort(view_poly2.begin(), view_poly2.end(),
+                    [this](Monomial<T, TNumberOfVariables> const& mon1,
+                           Monomial<T, TNumberOfVariables> const& mon2) {
+                      return mon_order_.is_less(mon1, mon2);
+                    });
+
+          auto it1 = view_poly1.rbegin();
+          auto it2 = view_poly2.rbegin();
+          while (it1 != view_poly1.rend() && it2 != view_poly2.rend()) {
+              if (mon_order_.is_less(*it1, *it2))
+                  return true;
+              if (mon_order_.is_less(*it2, *it1))
+                  return false;
+            ++it1;
+            ++it2;
+          }
+          return view_poly1.size() < view_poly2.size();
         }
 
     private:
