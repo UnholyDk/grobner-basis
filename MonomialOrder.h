f2c3f3f55f89021a1714de705d3c54d17f29c3da
Vlad <vladan1030@gmail.com>
2019-08-21 22:17:31 +0300

    Rename 'func' on to 'comparator'

--- MonomialOrder.h
+++ MonomialOrder.h
@@ -45,9 +45,9 @@ class MonomialOrder {
   }
 
   bool is_less(const monomial &mon1, const monomial &mon2) const {
-    for (auto func : comparators_) {
-      if (func(mon1, mon2)!=func(mon2, mon1)) {
-        return func(mon1, mon2);
+    for (auto comparator : comparators_) {
+      if (comparator(mon1, mon2)!=comparator(mon2, mon1)) {
+        return comparator(mon1, mon2);
       }
     }
     return false;
